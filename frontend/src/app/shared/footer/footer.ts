5d2ebf6a926140b0ff6ca4867f87d4ef1c8e344a
Gaultier Patrice <gaultiercode@gmail.com>
2026-04-29 16:16:01 +0200

    style: replaced text by logos on footer

--- frontend/src/app/shared/footer/footer.ts
+++ frontend/src/app/shared/footer/footer.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './footer.html',
+  styleUrl: './footer.css',
 })
 export class FooterComponent {}
