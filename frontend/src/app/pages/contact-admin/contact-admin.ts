a71f421734b9d7f10f5d14e9a1468a53c8f08d4e
Gaultier Patrice <gaultiercode@gmail.com>
2026-05-06 15:09:56 +0200

    style: added background on all pages and blur effect on landing & dashboard

--- frontend/src/app/pages/contact-admin/contact-admin.ts
+++ frontend/src/app/pages/contact-admin/contact-admin.ts
@@ -5,5 +5,20 @@ import { RouterModule } from '@angular/router';
   selector: 'app-contact-admin',
   imports: [RouterModule],
   templateUrl: './contact-admin.html',
+  styles: [`
+    .hero {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      flex: 1;
+      background-image: url('/assets/images/illustration/vegetables.jpg');
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      padding: 20px;
+    }
+  `]
 })
 export class ContactAdminComponent {}
